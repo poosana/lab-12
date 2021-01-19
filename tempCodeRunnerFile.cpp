00bd2c3f4d2fe42a9a5bfe556ee7d305b5a59c48
poosana <piixd1@gmail.com>
2021-01-19 14:40:50 +0700

    done

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,3 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-const int N = 30;
-const int M = 70;
-
-void updateImage(bool [][M],int,int,int);
-
-void showImage(const bool [][M]);
-
-int main()
-{
-    bool image[N][M] = {};
-    int s,x,y;
-    do{
-        showImage(image);
-        cout << "Input your brush size and location: ";
-        cin >> s >> x >> y;
-        updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
-    return 0;
-}
-
-
 void showImage(const bool image[N][M])
 {
     for(int i=0 ; i<M+2;i++) cout << "-";
@@ -37,8 +12,7 @@ void showImage(const bool image[N][M])
         cout << "|";
         cout <<"\n";
     }
-    for (int i = 0;i<M+2;i++){ cout << "-";}
-    cout<<"\n";
+    for (int i = 0;i<M+2;i++) cout << "-";
 }
 void updateImage(bool image[N][M],int s ,int x,int y)
 {
